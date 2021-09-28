e58c6259a3ae23bc0df7c396dcd951dfbd882d2c
Emanuele <dicio37@gmail.com>
2021-09-28 21:30:36 +0200

    feat: Added dependency tree node caching and cycle detection

--- src/services/redis.service.ts
+++ src/services/redis.service.ts
@@ -4,13 +4,15 @@ class RedisService {
   private static client: RedisClient;
 
   public static async init(): Promise<void> {
-    RedisService.client = createClient({ url: process.env.REDIS_URL });
+    RedisService.client = createClient({
+      url: process.env.REDIS_URL || 'redis://localhost:6379/',
+    });
     RedisService.client.on('error', (err) => {
       console.error('❌ Redis client error', err);
       process.exit(1);
     });
     await new Promise<void>((resolve) => {
-      RedisService.client.on('connect', () => {
+      this.client.on('connect', () => {
         console.info('🔌 Redis client connected');
         resolve();
       });
@@ -18,21 +20,21 @@ class RedisService {
   }
 
   public static async get(key: string): Promise<string | undefined> {
-    let value = null;
+    let value;
     await new Promise<void>((resolve) => {
-      RedisService.client.get(key, (error, data) => {
-        if (!error) value = data;
-        else console.error(error);
+      this.client.get(key, (error, data) => {
+        if (error) console.error(error);
+        if (data) value = data;
         resolve();
       });
     });
-    return value;
+    return value as string;
   }
 
   public static async set(key: string, value: string): Promise<void> {
     const expirationTime = parseInt(process.env.REDIS_EXP_TIME) || 60 * 60 * 24;
     await new Promise<void>((resolve) => {
-      RedisService.client.set(key, value, 'EX', expirationTime, (error) => {
+      this.client.set(key, value, 'EX', expirationTime, (error) => {
         if (error) console.error(error);
         resolve();
       });
