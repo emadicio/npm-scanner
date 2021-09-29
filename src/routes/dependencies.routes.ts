e36a7cd1b71f5b0a44f3d97d91852579307710b0
Emanuele <dicio37@gmail.com>
2021-09-29 18:10:07 +0200

    docs: Added documentation and notes

--- src/routes/dependencies.routes.ts
+++ src/routes/dependencies.routes.ts
@@ -3,11 +3,18 @@ import DependenciesController from '@controllers/dependencies.controller';
 
 const DependenciesRoutes = Router();
 
+// E.g. /dependencies/express
 DependenciesRoutes.get('/:packageName', DependenciesController.get);
+
+// E.g. /dependencies/express/4.17.0
+// or   /dependencies/express/latest
+// or   /dependencies/@loopback/rest
 DependenciesRoutes.get(
   '/:scopeOrPackageName/:packageNameOrVersion',
   DependenciesController.get,
 );
+
+// E.g. /dependencies/@loopback/rest/10.0.0
 DependenciesRoutes.get(
   '/:scope/:packageName/:version',
   DependenciesController.get,
